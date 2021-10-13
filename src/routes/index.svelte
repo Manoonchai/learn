031630b3e899832cc4a41980c98a8115cc79c553
Manassarn Manoonchai <manassarn@gmail.com>
2021-10-13 21:26:49 +0700

    Add Tailwindcss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let name = 'Manoonchai';
+</script>
+
+<main class="w-full h-screen flex flex-col gap-2 justify-center items-center">
+	<h1 class="text-6xl text-green-400 flex flex-col">Hello {name}</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
