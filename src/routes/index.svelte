e0684d3734bd353dcae2ab7542e360952e88ef57
Manassarn Manoonchai <manassarn@gmail.com>
2021-10-18 21:34:23 +0700

    Display wpm

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { calculateWpm } from '$lib/wpm'
+
   let name = 'Manoonchai'
   let input
   export let words = [
@@ -20,10 +22,29 @@
   let currentWordIdx
   let sentence
   let ended
+  let started
+  let elapsed
+  let startTime = new Date().getTime()
+  let correctWords = []
+  let interval
 
   reset()
 
+  function start() {
+    if (started) {
+      return
+    }
+
+    interval = setInterval(() => {
+      elapsed = (new Date().getTime() - startTime) / 1000
+    }, 1000)
+
+    started = true
+  }
+
   function onType(e) {
+    start()
+
     if (ended) {
       return e.preventDefault()
     }
@@ -33,8 +54,10 @@
 
       if (input) {
         if (sentence[currentWordIdx] === input) {
+          correctWords = correctWords.concat(input)
           result = result.concat(true)
         } else {
+          correctWords = correctWords.concat('')
           result = result.concat(false)
         }
 
@@ -49,22 +72,31 @@
   }
 
   function reset() {
+    started = false
     ended = false
     result = []
     currentWordIdx = 0
     sentence = Array(15)
       .fill(null)
       .map(() => words[Math.floor(Math.random() * words.length)])
+    startTime = new Date().getTime()
+    correctWords = []
   }
 
   function end() {
     ended = true
+    clearInterval(interval)
     alert('Good job!')
   }
 </script>
 
 <main class="container min-h-screen mx-auto flex flex-col gap-2 justify-center items-center">
   <h1 class="text-6xl text-green-400 flex flex-col">Hello {name}</h1>
+
+  <!-- <p>Started : {started}</p>
+  <p>Ended : {ended}</p> -->
+
+  <p>{calculateWpm(correctWords, elapsed).toFixed(1)} wpm</p>
   <p>
     {#each sentence as word, idx}
       <span
