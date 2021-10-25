5d996ae29d3fa165a5c7baabb12ce07f70aeafe7
Manassarn Manoonchai <manassarn@gmail.com>
2021-10-25 21:21:43 +0700

    Highlight next char in shift layer

--- src/lib/components/KeymapButton.svelte
+++ src/lib/components/KeymapButton.svelte
@@ -10,13 +10,14 @@
     @apply inline-block border;
     @apply flex flex-col justify-center items-center;
     @apply text-base w-8 h-8 border rounded;
-    @apply md:text-xl md:w-10 md:h-10 md:border-2 md:rounded;
+    @apply md:text-lg md:w-10 md:h-10 md:border-2 md:rounded;
     @apply lg:text-2xl lg:w-14 lg:h-14 lg:border-2 lg:rounded;
-    @apply xl:text-4xl xl:w-20 xl:h-20 xl:border-4 xl:rounded-xl;
+    @apply xl:text-3xl xl:w-20 xl:h-20 xl:border-4 xl:rounded-xl;
   }
 
   .key {
     @apply -mt-2;
+    @apply xl:mt-0;
   }
 
   .key-shift {
@@ -24,7 +25,7 @@
   }
 </style>
 
-<span class="button {keys[0] === nextChar ? 'bg-indigo-400 border-indigo-800' : ''}">
+<span class="button {keys.includes(nextChar) ? 'bg-indigo-400 border-indigo-800' : ''}">
   <div class="key-shift">{keys[1]}</div>
   <div class="key">{keys[0]}</div>
 </span>
