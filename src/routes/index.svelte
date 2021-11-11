f4aa47948014c897da997bbe623ea2e67e84250f
Manassarn Manoonchai <manassarn@gmail.com>
2021-11-11 21:51:08 +0700

    fix: Clear interval before restart

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,14 @@
   import Modal from '$lib/components/Modal.svelte'
   import LessonModal from '$lib/components/LessonModal.svelte'
   import Changelog from '$lib/components/Changelog.svelte'
-  import { showKeymap, showPrevOrNextWord, currentLessonName, TabToRestart, DarkMode, GlowKey } from '$lib/store'
+  import {
+    showKeymap,
+    showPrevOrNextWord,
+    currentLessonName,
+    TabToRestart,
+    DarkMode,
+    GlowKey,
+  } from '$lib/store'
 
   let name = 'Manoonchai'
   let input
@@ -60,13 +67,17 @@
       return
     }
 
+    if (interval) {
+      clearInterval(interval)
+    }
+
     interval = setInterval(() => {
       elapsed = (new Date().getTime() - startTime) / 1000
     }, 500)
 
     started = true
   }
-    
+
   function onType(e: KeyboardEvent) {
     e.preventDefault()
 
@@ -109,11 +120,11 @@
   }
 
   function reset() {
-      if ($currentLessonName) {
-        currentLesson = lessons.find((l) => l.name === $currentLessonName) || lessons[0]
-      } else {
-        currentLesson = lessons[0]
-      }
+    if ($currentLessonName) {
+      currentLesson = lessons.find((l) => l.name === $currentLessonName) || lessons[0]
+    } else {
+      currentLesson = lessons[0]
+    }
 
     words = currentLesson.words
 
@@ -122,7 +133,7 @@
     userType = []
     result = []
     currentWordIdx = 0
-    
+
     sentence = Array(30)
       .fill(null)
       .map(() => words[Math.floor(Math.random() * words.length)] || '')
@@ -135,10 +146,10 @@
   window.onkeydown = (e) => {
     e.preventDefault()
     if (e.key === 'Tab') {
-      if($TabToRestart === true) {
+      if ($TabToRestart === true) {
         reset()
       } else {
-        return;
+        return
       }
     }
   }
@@ -167,76 +178,113 @@
   />
 </svelte:head>
 
-<body class="{$DarkMode ? 'dark' : ''}">
-<main class="main container min-h-screen mx-auto flex dark:bg-black flex-col gap-2 justify-center items-center py-20">
-  <div class="title dark:text-white font-sarabun text-black flex flex-row font-bold"><img src="https://manoonchai.com/_next/image?url=%2Fmanoonchai.png&w=64&q=75" class="align-middle" width={64} height={64} alt="logo" /><span>Learn {name}</span></div>
+<body class={$DarkMode ? 'dark' : ''}>
+  <main
+    class="main container min-h-screen mx-auto flex dark:bg-black flex-col gap-2 justify-center items-center py-20"
+  >
+    <div class="title dark:text-white font-sarabun text-black flex flex-row font-bold">
+      <img
+        src="https://manoonchai.com/_next/image?url=%2Fmanoonchai.png&w=64&q=75"
+        class="align-middle"
+        width={64}
+        height={64}
+        alt="logo"
+      /><span>Learn {name}</span>
+    </div>
 
-  <p class="stat">{wpm} wpm</p>
-  <p class="sentence">
-    {#each sentence as word, idx (idx)}
-      <span
-        class="sentence-gap font-sarabun dark:text-white transition duration-200 break-word {idx === currentWordIdx
-          ? 'bg-green-300 dark:bg-green-500'
-          : ''}
+    <p class="stat">{wpm} wpm</p>
+    <p class="sentence">
+      {#each sentence as word, idx (idx)}
+        <span
+          class="sentence-gap font-sarabun dark:text-white transition duration-200 break-word {idx ===
+          currentWordIdx
+            ? 'bg-green-300 dark:bg-green-500'
+            : ''}
         {result[idx] === true ? 'text-green-400 dark:text-green-600' : ''}
         {result[idx] === false ? 'text-red-600 dark:text-red-600' : ''}
         {sentence[currentWordIdx] &&
-        userType.join('') !== sentence[currentWordIdx].slice(0, userType.length) &&
-        input &&
-        idx === currentWordIdx
-          ? 'bg-red-400 dark:bg-red-600'
-          : ''}
+          userType.join('') !== sentence[currentWordIdx].slice(0, userType.length) &&
+          input &&
+          idx === currentWordIdx
+            ? 'bg-red-400 dark:bg-red-600'
+            : ''}
         "
-      >
-        {word}
-      </span>
-    {/each}
-  </p>
+        >
+          {word}
+        </span>
+      {/each}
+    </p>
 
-  <div class="flex items-center">
-    {#if $showPrevOrNextWord}
-    <p class="flex-none word-next text-gray-200 dark:text-gray-600 w-32 text-xl pt-4 pl-6 font-sarabun">{sentence[currentWordIdx - 1] ?? ''}</p> 
-    {/if}
-    <input
-    class="input shadow-white dark:ring-offset-black border w-full font-sarabun shadow-lg rounded-lg border-gray-400 dark:border-gray-600 focus:ring-2
+    <div class="flex items-center">
+      {#if $showPrevOrNextWord}
+        <p
+          class="flex-none word-next text-gray-200 dark:text-gray-600 w-32 text-xl pt-4 pl-6 font-sarabun"
+        >
+          {sentence[currentWordIdx - 1] ?? ''}
+        </p>
+      {/if}
+      <input
+        class="input shadow-white dark:ring-offset-black border w-full font-sarabun shadow-lg rounded-lg border-gray-400 dark:border-gray-600 focus:ring-2
     ring-offset-2 ring-green-400 transition duration-200 {!currentWordSpellCheck
-      ? 'bg-red-400 ring-red-400'
-      : ''}"
-    value={input}
-    on:keydown={onType}
-    placeholder={sentence[currentWordIdx]}
-    data-testid="input"
-    bind:this={typingInput}
-  />
-  {#if $showPrevOrNextWord}
-  <p class="flex-none word-next text-gray-400 dark:text-gray-200 w-32 text-xl pt-4 pl-6 font-sarabun">{sentence[currentWordIdx + 1] ?? ''}</p> 
-  {/if}  
-</div>
+          ? 'bg-red-400 ring-red-400'
+          : ''}"
+        value={input}
+        on:keydown={onType}
+        placeholder={sentence[currentWordIdx]}
+        data-testid="input"
+        bind:this={typingInput}
+      />
+      {#if $showPrevOrNextWord}
+        <p
+          class="flex-none word-next text-gray-400 dark:text-gray-200 w-32 text-xl pt-4 pl-6 font-sarabun"
+        >
+          {sentence[currentWordIdx + 1] ?? ''}
+        </p>
+      {/if}
+    </div>
 
-  {#if $showKeymap}
-    <Keymap {nextChar} />
-  {/if}
+    {#if $showKeymap}
+      <Keymap {nextChar} />
+    {/if}
 
-  <button class="btn hover:bg-gray-600 rounded-lg transition duration-300 m-2 dark:text-white" on:click={reset}>
-    Reset
-  </button>
+    <button
+      class="btn hover:bg-gray-600 rounded-lg transition duration-300 m-2 dark:text-white"
+      on:click={reset}
+    >
+      Reset
+    </button>
 
-  <div class="lesson dark:text-white font-sarabun">
-    Lesson: <span class="p-2 cursor-pointer border border-gray-400 dark:text-white rounded-lg" on:click={() => (showLesson = true)}>{$currentLessonName}</span>
-  {#if showLesson}
-    <LessonModal closeModal={() => {(showLesson = false); reset()}} />
-  {/if}
-  </div>
-  <Footer bind:showMenu bind:showChangelog />
+    <div class="lesson dark:text-white font-sarabun">
+      Lesson: <span
+        class="p-2 cursor-pointer border border-gray-400 dark:text-white rounded-lg"
+        on:click={() => (showLesson = true)}>{$currentLessonName}</span
+      >
+      {#if showLesson}
+        <LessonModal
+          closeModal={() => {
+            showLesson = false
+            reset()
+          }}
+        />
+      {/if}
+    </div>
+    <Footer bind:showMenu bind:showChangelog />
 
-  {#if showMenu}
-    <Modal closeModal={() => (showMenu = false)} bind:showKeymap={$showKeymap} bind:showPrevOrNextWord={$showPrevOrNextWord} bind:TabToRestart={$TabToRestart} bind:DarkMode={$DarkMode} bind:GlowKey={$GlowKey} />
-  {/if} 
+    {#if showMenu}
+      <Modal
+        closeModal={() => (showMenu = false)}
+        bind:showKeymap={$showKeymap}
+        bind:showPrevOrNextWord={$showPrevOrNextWord}
+        bind:TabToRestart={$TabToRestart}
+        bind:DarkMode={$DarkMode}
+        bind:GlowKey={$GlowKey}
+      />
+    {/if}
 
-  {#if showChangelog}
-    <Changelog closeModal={() => (showChangelog = false)} />
-  {/if}
-</main>
+    {#if showChangelog}
+      <Changelog closeModal={() => (showChangelog = false)} />
+    {/if}
+  </main>
 </body>
 
 <Kofi name="narze" />
