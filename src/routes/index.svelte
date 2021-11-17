ef4c6eef0965b1ef683599eb431bc5c119ab2ff4
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-11-17 18:26:35 +0700

    Add modal overlay

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -276,26 +276,28 @@
     </div>
     {#if showWpm}
       <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-          <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-          <div class="popin inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-            <div class="bg-white dark:bg-black px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-              <div class="text-center sm:text-left">
-                <h1 class="text-4xl leading-6 font-medium text-gray-900 dark:text-gray-100 text-center" id="modal-title">
-                  You get {wpm} wpm
-                </h1>
-                <p class="mt-4 dark:text-gray-100">Lesson : {$currentLessonName}</p>
-                <div class="bg-white dark:bg-black px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                  <button
-                    type="button"
-                    class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-400 text-base font-medium text-white hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 dark:ring-offset-black focus:ring-green-300 sm:ml-3 sm:w-auto sm:text-sm"
-                    on:click={close}
-                  >
-                    Close
-                  </button>
+        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-">
+          <div class="fixed fadein inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true" on:click={close}>
+            <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+              <div class="popin inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div class="bg-white dark:bg-black px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div class="text-center sm:text-left">
+                    <h1 class="text-4xl leading-6 font-medium text-gray-900 dark:text-gray-100 text-center" id="modal-title">
+                      You get {wpm} wpm
+                    </h1>
+                    <p class="mt-4 dark:text-gray-100">Lesson : {$currentLessonName}</p>
+                    <div class="bg-white dark:bg-black px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                      <button
+                        type="button"
+                        class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-400 text-base font-medium text-white hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 dark:ring-offset-black focus:ring-green-300 sm:ml-3 sm:w-auto sm:text-sm"
+                        on:click={close}
+                      >
+                        Close
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
           </div>
         </div>
       </div>
